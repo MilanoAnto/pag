9e416f281e59c0d9061e17b3ef237d8e57dfa23d
MilanoAnto <milanoanto04@gmail.com>
2023-10-11 21:28:52 -0300

    uuuu

--- bd.php
+++ bd.php
@@ -1,25 +1,36 @@
 <?php
 
-$servername = "localhost"; 
+$servername = "localhost";
 $username = "anto";
 $password = "Lara1983-";
 $database = "pag";
 
+// Crear una conexión
 $conn = new mysqli($servername, $username, $password, $database);
 
+// Verificar la conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+
+// Obtener los valores del formulario de manera segura
 $nombre = $_POST['nombre'];
 $apellido = $_POST['apellido'];
 $mail = $_POST['mail'];
 $usuario = $_POST['usuario'];
 $contraseña = $_POST['contraseña'];
 
-$sql = "INSERT INTO Usuarios (nombre, apellido, mail, usuario, contraseña) VALUES ('$usuario', '$nombre', '$apellido', '$mail',  '$contraseña')";
+// Utilizar una sentencia preparada para proteger contra inyecciones de SQL
+$stmt = $conn->prepare("INSERT INTO Usuarios (nombre, apellido, mail, usuario, contraseña) VALUES (?, ?, ?, ?, ?)");
+$stmt->bind_param("sssss", $nombre, $apellido, $mail, $usuario, $contraseña);
 
-if ($conn->query($sql)) {
+if ($stmt->execute()) {
     echo "Registro exitoso";
 } else {
-    echo "Error al registrar: " . $conn->error;
+    echo "Error al registrar: " . $stmt->error;
 }
 
+// Cerrar la conexión y liberar los recursos
+$stmt->close();
 $conn->close();
 ?>
