314d2da03e702207c74c271e29b1c9d059bcb1d1
MilanoAnto <milanoanto04@gmail.com>
2023-10-25 19:10:14 -0300

    333

--- bd.php
+++ bd.php
@@ -6,32 +6,27 @@ $username = "anto";
 $password = "Lara1983-";
 $database = "pag";
 
-// Crear una conexión
 $conn = new mysqli($servername, $username, $password, $database);
 
-// Verificar la conexión
 if ($conn->connect_error) {
+    // Registrar el error en un archivo de registro
     die("Conexión fallida: " . $conn->connect_error);
 }
 
-// Obtener los valores del formulario de manera segura
-$nombre = $_POST['nombre'];
-$apellido = $_POST['apellido'];
-$mail = $_POST['mail'];
-$usuario = $_POST['usuario'];
-$contraseña = $_POST['contraseña'];
+$nombre = mysqli_real_escape_string($conn, $_POST['nombre']);
+$apellido = mysqli_real_escape_string($conn, $_POST['apellido']);
+$mail = mysqli_real_escape_string($conn, $_POST['mail']);
+$usuario = mysqli_real_escape_string($conn, $_POST['usuario']);
+$contraseña = mysqli_real_escape_string($conn, $_POST['contraseña']);
 
-// Utilizar una sentencia preparada para proteger contra inyecciones de SQL
-$stmt = $conn->prepare("INSERT INTO Usuarios (nombre, apellido, mail, usuario, contraseña) VALUES (nombre, apellido, mail, usuario, contraseña)");
-$stmt->bind_param("sssss", $nombre, $apellido, $mail, $usuario, $contraseña);
+$sql = "INSERT INTO usuarios (nombre, apellido, mail, usuario, contrasena) VALUES ('$nombre', '$apellido', '$mail', '$usuario', '$contraseña')";
 
-if ($stmt->execute()) {
+if ($conn->query($sql) === TRUE) {
     echo "Registro exitoso";
 } else {
-    echo "Error al registrar: " . $stmt->error;
+    // Registrar el error en un archivo de registro
+    echo "Error al registrar: " . $conn->error;
 }
 
-// Cerrar la conexión y liberar los recursos
-$stmt->close();
 $conn->close();
 ?>
